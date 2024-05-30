ab5fd4573c5fe69ab46424d9f7d8b0ff195b2efb
Vakho <v.zhgenti@csgo.com>
2024-05-30 14:09:12 +0300

    small improvements

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const tokenController = require("./src/controllers/tokenController");
 
 const app = express();
+const config = require("./src/config/index");
 
 app.get("/token", tokenController.getTokenInfo);
 
-app.listen(4000, () => console.log('App is up and working on port 4000'))
+app.listen(config.port, () => console.log(`App is up and working on port ${config.port}`));
